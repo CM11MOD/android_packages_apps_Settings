83ff767009df7dfedeed1719acb118479d5b6ca3
burstlam <burstlam@gmail.com>
2014-04-13 12:11:34 +0800

    Prepare for new theme engine
    Conflicts:
    	res/xml/settings_headers.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1129,11 +1129,11 @@ public class Settings extends PreferenceActivity
 
         static int getHeaderType(Header header) {
             if (header.fragment == null && header.intent == null
-                    && header.id != R.id.theme_settings) {
+                    && header.id != R.id.slim_theme_settings) {
                 return HEADER_TYPE_CATEGORY;
             } else if (header.id == R.id.wifi_settings
                     || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.theme_settings
+                    || header.id == R.id.slim_theme_settings
                     || header.id == R.id.mobile_network_settings
                     || header.id == R.id.profiles_settings
                     || header.id == R.id.power_saver_mode
@@ -1272,7 +1272,7 @@ public class Settings extends PreferenceActivity
                         mMobileDataEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.profiles_settings) {
                         mProfileEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.theme_settings) {
+                    } else if (header.id == R.id.slim_theme_settings) {
                         mThemeEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.location_settings) {
                         mLocationEnabler.setSwitch(holder.switch_);
