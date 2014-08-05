1ffaf23c60d8a73734bf203debd0fdf15f92073d
burstlam <burstlam@gmail.com>
2014-08-05 11:12:11 +0800

    Hover : rework UI

--- src/com/android/settings/pa/Hover.java
+++ src/com/android/settings/pa/Hover.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2014 The LiquidSmooth Project
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,34 +16,40 @@
 
 package com.android.settings.pa;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.Gravity;
-
-import com.android.internal.util.slim.DeviceUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import com.android.settings.util.Helpers;
 
-public class Hover extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+public class Hover extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "HoverSettings";
     private static final String PREF_HOVER_LONG_FADE_OUT_DELAY = "hover_long_fade_out_delay";
@@ -52,10 +58,23 @@ public class Hover extends SettingsPreferenceFragment implements
     ListPreference mHoverLongFadeOutDelay;
     ListPreference mHoverMicroFadeOutDelay;
 
+    private Switch mActionBarSwitch;
+    private HoverEnabler mHoverEnabler;
+
+    private ViewGroup mPrefsContainer;
+    private View mDisabledText;
+
+    private ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updateEnabledState();
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // Get launch-able applications
         addPreferencesFromResource(R.xml.hover_settings);
 
         PreferenceScreen prefSet = getPreferenceScreen();
@@ -74,24 +93,79 @@ public class Hover extends SettingsPreferenceFragment implements
         mHoverMicroFadeOutDelay.setSummary(mHoverMicroFadeOutDelay.getEntry());
         mHoverMicroFadeOutDelay.setOnPreferenceChangeListener(this);
 
-        //
+    }
 
-        UpdateSettings();
+    @Override
+    public void onActivityCreated(Bundle icicle) {
+        // We don't call super.onActivityCreated() here, since it assumes we already set up
+        // Preference (probably in onCreate()), while ProfilesSettings exceptionally set it up in
+        // this method.
+        // On/off switch
+        Activity activity = getActivity();
+        //Switch
+        mActionBarSwitch = new Switch(activity);
+
+        if (activity instanceof PreferenceActivity) {
+            PreferenceActivity preferenceActivity = (PreferenceActivity) activity;
+            if (preferenceActivity.onIsHidingHeaders() || !preferenceActivity.onIsMultiPane()) {
+                final int padding = activity.getResources().getDimensionPixelSize(
+                        R.dimen.action_bar_switch_padding);
+                mActionBarSwitch.setPaddingRelative(0, 0, padding, 0);
+                activity.getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
+                        ActionBar.DISPLAY_SHOW_CUSTOM);
+                activity.getActionBar().setCustomView(mActionBarSwitch, new ActionBar.LayoutParams(
+                        ActionBar.LayoutParams.WRAP_CONTENT,
+                        ActionBar.LayoutParams.WRAP_CONTENT,
+                        Gravity.CENTER_VERTICAL | Gravity.END));
+            }
+        }
+
+        mHoverEnabler = new HoverEnabler(activity, mActionBarSwitch);
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(icicle);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.hover_fragment, container, false);
+        mPrefsContainer = (ViewGroup) v.findViewById(R.id.prefs_container);
+        mDisabledText = v.findViewById(R.id.disabled_text);
+
+        View prefs = super.onCreateView(inflater, mPrefsContainer, savedInstanceState);
+        mPrefsContainer.addView(prefs);
+
+        return v;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        UpdateSettings();
+        if (mHoverEnabler != null) {
+            mHoverEnabler.resume();
+        }
+        getActivity().invalidateOptionsMenu();
+
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.HOVER_ENABLED),
+                true, mSettingsObserver);
+        updateEnabledState();
+
+        // If running on a phone, remove padding around container
+        // and the preference listview
+        if (!Utils.isTablet(getActivity())) {
+            mPrefsContainer.setPadding(0, 0, 0, 0);
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-    }
-
-    public void UpdateSettings() {
-        //
+        if (mHoverEnabler != null) {
+            mHoverEnabler.pause();
+        }
+        getContentResolver().unregisterContentObserver(mSettingsObserver);
     }
 
     @Override
@@ -116,8 +190,10 @@ public class Hover extends SettingsPreferenceFragment implements
         return false;
     }
 
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    private void updateEnabledState() {
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.HOVER_ENABLED, 0) != 0;
+        mPrefsContainer.setVisibility(enabled ? View.VISIBLE : View.GONE);
+        mDisabledText.setVisibility(enabled ? View.GONE : View.VISIBLE);
     }
 }
