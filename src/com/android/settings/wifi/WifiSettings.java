143e0a08a9dc3f23e61fdac35ae298508fb7cb75
Frederic Danis <frederic.danis@intel.com>
2014-08-11 11:05:17 +0200

    Update AP signal strength on signal level changed
    Patch related to bug GMIN-1381 "The AP signal strength is display
    different in notification bar and WIFI settings"
    
    On behalf of Ernest P Chen:
    Update all access points when reacting to a broadcast that the signal
    level changed.
    
    Change-Id: If690212b9d4640d236747eacef9a4ac8c4c98cd9
    Signed-off-by: Frederic Danis <frederic.danis@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- src/com/android/settings/wifi/WifiSettings.java
+++ src/com/android/settings/wifi/WifiSettings.java
@@ -885,6 +885,7 @@ public class WifiSettings extends RestrictedSettingsFragment
                 return;
             }
         } else if (WifiManager.RSSI_CHANGED_ACTION.equals(action)) {
+            updateAccessPoints();
             updateConnectionState(null);
         }
     }
