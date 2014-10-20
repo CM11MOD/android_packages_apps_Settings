2f13a0e695c8a4ae215cdbb946078caad1351cc1
burstlam <burstlam@gmail.com>
2014-10-21 03:28:54 +0800

    Revert "Move Notification Reminder"
    This reverts commit 2c856352ff26963e55534d0830c9e468df5ce417.

--- src/com/android/settings/slim/NotificationService.java
+++ src/com/android/settings/slim/NotificationService.java
@@ -5,14 +5,10 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.net.Uri;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.RemoteException;
 import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
@@ -20,12 +16,9 @@ import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
-import android.preference.RingtonePreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-
-import java.net.URISyntaxException;
 
 public class NotificationService extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -39,19 +32,11 @@ public class NotificationService extends SettingsPreferenceFragment implements
     private static final String PREF_HOVER = "hover_settings";
     private static final String PREF_HEADSUP = "heads_up_enabled";
 
+
     private static final String KEY_LIGHT_OPTIONS = "category_light_options";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_NOTIFICATION_LIGHT = "notification_light";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
-
-    private static final String PREF_NOTI_REMINDER_SOUND =
-            "noti_reminder_sound";
-    private static final String PREF_NOTI_REMINDER_ENABLED =
-            "noti_reminder_enabled";
-    private static final String PREF_NOTI_REMINDER_INTERVAL =
-            "noti_reminder_interval";
-    private static final String PREF_NOTI_REMINDER_RINGTONE =
-            "noti_reminder_ringtone";
 
     private PreferenceGroup mNotification;
     private PreferenceGroup mLockNotification;
@@ -64,11 +49,6 @@ public class NotificationService extends SettingsPreferenceFragment implements
     private PreferenceCategory mLightOptions;
     private PreferenceScreen mNotificationLight;
     private PreferenceScreen mBatteryPulse;
-
-    private CheckBoxPreference mReminder;
-    private ListPreference mReminderInterval;
-    private ListPreference mReminderMode;
-    private RingtonePreference mReminderRingtone;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -129,43 +109,6 @@ public class NotificationService extends SettingsPreferenceFragment implements
                 prefSet.removePreference(mLightOptions);
             }
         }
-
-
-        mReminder = (CheckBoxPreference) findPreference(PREF_NOTI_REMINDER_ENABLED);
-        mReminder.setChecked(Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
-        mReminder.setOnPreferenceChangeListener(this);
-
-        mReminderInterval = (ListPreference) findPreference(PREF_NOTI_REMINDER_INTERVAL);
-        int interval = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_INTERVAL, 0, UserHandle.USER_CURRENT);
-        mReminderInterval.setOnPreferenceChangeListener(this);
-        updateReminderIntervalSummary(interval);
-
-        mReminderMode = (ListPreference) findPreference(PREF_NOTI_REMINDER_SOUND);
-        int mode = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_NOTIFY, 0, UserHandle.USER_CURRENT);
-        mReminderMode.setValue(String.valueOf(mode));
-        mReminderMode.setOnPreferenceChangeListener(this);
-        updateReminderModeSummary(mode);
-
-        mReminderRingtone =
-                (RingtonePreference) findPreference(PREF_NOTI_REMINDER_RINGTONE);
-        Uri ringtone = null;
-        String ringtoneString = Settings.System.getStringForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_RINGER, UserHandle.USER_CURRENT);
-        if (ringtoneString == null) {
-            // Value not set, defaults to Default Ringtone
-            ringtone = RingtoneManager.getDefaultUri(
-                    RingtoneManager.TYPE_RINGTONE);
-        } else {
-            ringtone = Uri.parse(ringtoneString);
-        }
-        Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
-        mReminderRingtone.setSummary(alert.getTitle(getActivity()));
-        mReminderRingtone.setOnPreferenceChangeListener(this);
-        mReminderRingtone.setEnabled(mode != 0);
-
         updateLightPulseDescription();
         updateBatteryPulseDescription();
     }
@@ -192,36 +135,7 @@ public class NotificationService extends SettingsPreferenceFragment implements
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mReminder) {
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_ENABLED,
-                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mReminderInterval) {
-            int interval = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_INTERVAL,
-                    interval, UserHandle.USER_CURRENT);
-            updateReminderIntervalSummary(interval);
-        } else if (preference == mReminderMode) {
-            int mode = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_NOTIFY,
-                    mode, UserHandle.USER_CURRENT);
-            updateReminderModeSummary(mode);
-            mReminderRingtone.setEnabled(mode != 0);
-            return true;
-        } else if (preference == mReminderRingtone) {
-            Uri val = Uri.parse((String) newValue);
-            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), val);
-            mReminderRingtone.setSummary(ringtone.getTitle(getActivity()));
-            Settings.System.putStringForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_RINGER,
-                    val.toString(), UserHandle.USER_CURRENT);
-            return true;
-        }
         return false;
     }
 
@@ -301,49 +215,4 @@ public class NotificationService extends SettingsPreferenceFragment implements
         }
      }
 
-
-    private void updateReminderIntervalSummary(int value) {
-        int resId;
-        switch (value) {
-            case 1000:
-                resId = R.string.noti_reminder_interval_1s;
-                break;
-            case 2000:
-                resId = R.string.noti_reminder_interval_2s;
-                break;
-            case 2500:
-                resId = R.string.noti_reminder_interval_2dot5s;
-                break;
-            case 3000:
-                resId = R.string.noti_reminder_interval_3s;
-                break;
-            case 3500:
-                resId = R.string.noti_reminder_interval_3dot5s;
-                break;
-            case 4000:
-                resId = R.string.noti_reminder_interval_4s;
-                break;
-            default:
-                resId = R.string.noti_reminder_interval_1dot5s;
-                break;
-        }
-        mReminderInterval.setValue(Integer.toString(value));
-        mReminderInterval.setSummary(getResources().getString(resId));
-    }
-
-    private void updateReminderModeSummary(int value) {
-        int resId;
-        switch (value) {
-            case 1:
-                resId = R.string.enabled;
-                break;
-            case 2:
-                resId = R.string.noti_reminder_sound_looping;
-                break;
-            default:
-                resId = R.string.disabled;
-                break;
-        }
-        mReminderMode.setSummary(getResources().getString(resId));
-    }
 }
