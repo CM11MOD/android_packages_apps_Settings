2c856352ff26963e55534d0830c9e468df5ce417
burstlam <burstlam@gmail.com>
2014-10-21 03:28:28 +0800

    Move Notification Reminder

--- src/com/android/settings/slim/NotificationDrawerSettings.java
+++ src/com/android/settings/slim/NotificationDrawerSettings.java
@@ -25,8 +25,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -35,7 +33,6 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.preference.RingtonePreference;
 import android.provider.Settings;
 import android.os.UserHandle;
 import android.view.LayoutInflater;
@@ -66,14 +63,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
             "notification_hide_labels";
     private static final String PREF_NOTIFICATION_ALPHA =
             "notification_alpha";
-    private static final String PREF_NOTI_REMINDER_SOUND =
-            "noti_reminder_sound";
-    private static final String PREF_NOTI_REMINDER_ENABLED =
-            "noti_reminder_enabled";
-    private static final String PREF_NOTI_REMINDER_INTERVAL =
-            "noti_reminder_interval";
-    private static final String PREF_NOTI_REMINDER_RINGTONE =
-            "noti_reminder_ringtone";
 
     private static final String PREF_NOTIFICATION_CW_LABEL_COLOR =
             "notification_carrier_wifi_label_color";
@@ -91,10 +80,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
 
     ListPreference mHideLabels;
     SeekBarPreference mNotificationAlpha;
-    CheckBoxPreference mReminder;
-    ListPreference mReminderInterval;
-    ListPreference mReminderMode;
-    RingtonePreference mReminderRingtone;
     private AppSelectListPreference mClockShortcut;
     private AppSelectListPreference mCalendarShortcut;
 
@@ -154,41 +139,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
                 getPreferenceScreen().removePreference(mTabletDrawer);
             }
         }
-
-        mReminder = (CheckBoxPreference) findPreference(PREF_NOTI_REMINDER_ENABLED);
-        mReminder.setChecked(Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
-        mReminder.setOnPreferenceChangeListener(this);
-
-        mReminderInterval = (ListPreference) findPreference(PREF_NOTI_REMINDER_INTERVAL);
-        int interval = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_INTERVAL, 0, UserHandle.USER_CURRENT);
-        mReminderInterval.setOnPreferenceChangeListener(this);
-        updateReminderIntervalSummary(interval);
-
-        mReminderMode = (ListPreference) findPreference(PREF_NOTI_REMINDER_SOUND);
-        int mode = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_NOTIFY, 0, UserHandle.USER_CURRENT);
-        mReminderMode.setValue(String.valueOf(mode));
-        mReminderMode.setOnPreferenceChangeListener(this);
-        updateReminderModeSummary(mode);
-
-        mReminderRingtone =
-                (RingtonePreference) findPreference(PREF_NOTI_REMINDER_RINGTONE);
-        Uri ringtone = null;
-        String ringtoneString = Settings.System.getStringForUser(getContentResolver(),
-                Settings.System.REMINDER_ALERT_RINGER, UserHandle.USER_CURRENT);
-        if (ringtoneString == null) {
-            // Value not set, defaults to Default Ringtone
-            ringtone = RingtoneManager.getDefaultUri(
-                    RingtoneManager.TYPE_RINGTONE);
-        } else {
-            ringtone = Uri.parse(ringtoneString);
-        }
-        Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
-        mReminderRingtone.setSummary(alert.getTitle(getActivity()));
-        mReminderRingtone.setOnPreferenceChangeListener(this);
-        mReminderRingtone.setEnabled(mode != 0);
 
         mClockShortcut = (AppSelectListPreference)prefs.findPreference(CLOCK_SHORTCUT);
         mClockShortcut.setOnPreferenceChangeListener(this);
@@ -287,33 +237,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
             Settings.System.putFloat(getContentResolver(),
                     Settings.System.NOTIFICATION_ALPHA, valNav / 100);
             return true;
-        } else if (preference == mReminder) {
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_ENABLED,
-                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mReminderInterval) {
-            int interval = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_INTERVAL,
-                    interval, UserHandle.USER_CURRENT);
-            updateReminderIntervalSummary(interval);
-        } else if (preference == mReminderMode) {
-            int mode = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_NOTIFY,
-                    mode, UserHandle.USER_CURRENT);
-            updateReminderModeSummary(mode);
-            mReminderRingtone.setEnabled(mode != 0);
-            return true;
-        } else if (preference == mReminderRingtone) {
-            Uri val = Uri.parse((String) newValue);
-            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), val);
-            mReminderRingtone.setSummary(ringtone.getTitle(getActivity()));
-            Settings.System.putStringForUser(getContentResolver(),
-                    Settings.System.REMINDER_ALERT_RINGER,
-                    val.toString(), UserHandle.USER_CURRENT);
-            return true;
         } else if (preference == mClockShortcut) {
             String value = (String) newValue;
             // a value of null means to use the default
@@ -356,51 +279,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
         text.append(" " + res.getString(R.string.notification_hide_labels_text));
 
         mHideLabels.setSummary(text.toString());
-    }
-
-    private void updateReminderIntervalSummary(int value) {
-        int resId;
-        switch (value) {
-            case 1000:
-                resId = R.string.noti_reminder_interval_1s;
-                break;
-            case 2000:
-                resId = R.string.noti_reminder_interval_2s;
-                break;
-            case 2500:
-                resId = R.string.noti_reminder_interval_2dot5s;
-                break;
-            case 3000:
-                resId = R.string.noti_reminder_interval_3s;
-                break;
-            case 3500:
-                resId = R.string.noti_reminder_interval_3dot5s;
-                break;
-            case 4000:
-                resId = R.string.noti_reminder_interval_4s;
-                break;
-            default:
-                resId = R.string.noti_reminder_interval_1dot5s;
-                break;
-        }
-        mReminderInterval.setValue(Integer.toString(value));
-        mReminderInterval.setSummary(getResources().getString(resId));
-    }
-
-    private void updateReminderModeSummary(int value) {
-        int resId;
-        switch (value) {
-            case 1:
-                resId = R.string.enabled;
-                break;
-            case 2:
-                resId = R.string.noti_reminder_sound_looping;
-                break;
-            default:
-                resId = R.string.disabled;
-                break;
-        }
-        mReminderMode.setSummary(getResources().getString(resId));
     }
 
     private void updateClockCalendarSummary() {
